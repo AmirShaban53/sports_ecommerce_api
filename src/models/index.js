70afa60db3857d22395c34f6fbe4e271a8a25c60
unknown <amirshaban53@gmail.com>
2022-07-13 22:32:07 +0300

    config heroku

--- src/models/index.js
+++ src/models/index.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const devConfig = `postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DEV_DATABASE}`;
 const testConfig = `postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_TEST_DATABASE}`;
+const productionConifg = process.env.DATABASE_URL;
 
 const pool = {
   min: 0,
@@ -25,6 +26,11 @@ const dbConnect = async () => {
     if (process.env.NODE_ENV === "test") {
       sequelize = new Sequelize(testConfig, { pool: pool, logging: false });
       logger.info("connected to test database");
+    } else if (process.env.NODE_ENV === "production") {
+      sequelize = new Sequelize(productionConifg, {
+        pool: pool,
+        dialectOptions: { ssl: { rejectUnauthorized: false } },
+      });
     } else {
       sequelize = new Sequelize(devConfig, { pool: pool, logging: false });
       logger.info("connected to dev database");
