b8bcc4fc20b4eb5021b9b2b0d051e43c08b74a6b
unknown <amirshaban53@gmail.com>
2022-07-16 14:32:08 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 import Express from "express";
-// import cors from "cors";
-// import logger from "./middleware/logger";
-// import { cloudinaryConfig } from "./Config/cloudinaryConfig";
+import cors from "cors";
+import logger from "./middleware/logger";
+import { cloudinaryConfig } from "./Config/cloudinaryConfig";
 
-// import Categories from "./routes/categories";
-// import Products from "./routes/products";
-// import Checkout from "./routes/checkout";
-// import Users from "./routes/users";
-// import Cart from "./routes/cart";
+import Categories from "./routes/categories";
+import Products from "./routes/products";
+import Checkout from "./routes/checkout";
+import Users from "./routes/users";
+import Cart from "./routes/cart";
 
 const PORT = process.env.PORT || 5000;
 
 const app = Express();
 
-// app.use(Express.json());
-// app.use(cors());
-// app.use("*", cloudinaryConfig);
+app.use(Express.json());
+app.use(cors());
+app.use("*", cloudinaryConfig);
 
 app.get("/", (req, res) => {
   res.send("welcome to the sports ecommerce API");
 });
-// app.use("/uploads", Express.static("uploads"));
+app.use("/uploads", Express.static("uploads"));
 
-// app.use("/categories", Categories);
-// app.use("/products", Products);
-// app.use("/checkout", Checkout);
-// app.use("/users", Users);
-// app.use("/cart", Cart);
+app.use("/categories", Categories);
+app.use("/products", Products);
+app.use("/checkout", Checkout);
+app.use("/users", Users);
+app.use("/cart", Cart);
 
-// app.use((req, res) => {
-//   res.status(404).json({ message: "route not found!" });
-// });
+app.use((req, res) => {
+  res.status(404).json({ message: "route not found!" });
+});
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
 });
 
-// export default app;
+export default app;
