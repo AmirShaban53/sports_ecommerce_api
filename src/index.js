b65fba50886b1b8a82f2ee6d5b96e83a9f27d3e4
unknown <amirshaban53@gmail.com>
2022-07-16 04:26:23 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import express from "express";
+import Express from "express";
+
+
 import cors from "cors";
 import logger from "./middleware/logger";
 import { cloudinaryConfig } from "./Config/cloudinaryConfig";
@@ -11,16 +13,16 @@ import Cart from "./routes/cart";
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+const app = Express();
 
-app.use(express.json());
+app.use(Express.json());
 app.use(cors());
 app.use('*', cloudinaryConfig)
 
 app.get("/", (req, res) => {
   res.json("welcome to the sports ecommerce API");
 });
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", Express.static("uploads"));
 
 app.use("/categories", Categories);
 app.use("/products", Products);
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as categories from "../controllers/categories";
+
 const router = Router();
 
 router.get("/", categories.listCategories);
